0110261f80ad060b319ef2cf3b0b6a01fc7b9f03
ivan <ivanongjiaen@gmail.com>
2022-05-21 16:35:52 +0800

    finish chap07

--- com/craftinginterpreters/lox/Lox.java
+++ com/craftinginterpreters/lox/Lox.java
@@ -9,12 +9,14 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class Lox {
+  private static final Interpreter interpreter = new Interpreter();
   static boolean hadError = false;
+  static boolean hadRuntimeError = false;
 
   public static void main(String[] args) throws IOException {
     if (args.length > 1) {
       System.out.println("Usage: jlox [script]");
-      System.exit(64); 
+      System.exit(64);
     } else if (args.length == 1) {
       runFile(args[0]);
     } else {
@@ -25,16 +27,20 @@ public class Lox {
   private static void runFile(String path) throws IOException {
     byte[] bytes = Files.readAllBytes(Paths.get(path));
     run(new String(bytes, Charset.defaultCharset()));
-    if (hadError) System.exit(65);
+    if (hadError)
+      System.exit(65);
+    if (hadRuntimeError)
+      System.exit(70);
   }
 
   private static void runPrompt() throws IOException {
     BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-    for(;;) {
+    for (;;) {
       System.out.print("> ");
       String line = reader.readLine();
-      if (line == null) break;
+      if (line == null)
+        break;
       run(line);
       hadError = false;
     }
@@ -47,9 +53,10 @@ public class Lox {
     Parser parser = new Parser(tokens);
     Expr expression = parser.parse();
 
-    if (hadError) return;
+    if (hadError)
+      return;
 
-    System.out.println(new AstPrinter().print(expression));
+   interpreter.interpret(expression);
 
   }
 
@@ -58,7 +65,7 @@ public class Lox {
   }
 
   private static void report(int line, String where, String message) {
-    System.err.println( "[line " + line + "] Error" + where + ": " + message);
+    System.err.println("[line " + line + "] Error" + where + ": " + message);
     hadError = true;
   }
 
@@ -68,6 +75,12 @@ public class Lox {
     } else {
       report(token.line, " at '" + token.lexeme + "'", message);
     }
+  }
+
+  static void runtimeError(RuntimeError error) {
+    System.err.println(error.getMessage() +
+        "\n[line " + error.token.line + "]");
+    hadRuntimeError = true;
   }
 
 }
